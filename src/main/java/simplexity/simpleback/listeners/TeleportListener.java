164f4eabd045c12984071820877f178a1ec143c8
Peashooter101 <Peashooter101@yahoo.com>
2025-05-18 12:17:40 -0500

    Consider teleport cause for /back

--- src/main/java/simplexity/simpleback/listeners/TeleportListener.java
+++ src/main/java/simplexity/simpleback/listeners/TeleportListener.java
@@ -7,10 +7,12 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import simplexity.simpleback.SimpleBack;
+import simplexity.simpleback.config.ConfigHandler;
 
 public class TeleportListener implements Listener {
-    @EventHandler(priority= EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onTeleport(PlayerTeleportEvent event) {
+        if (!ConfigHandler.getInstance().getTeleportCauses().contains(event.getCause())) return;
         Player player = event.getPlayer();
         Location previousLocation = event.getFrom();
         SimpleBack.getInstance().getBackLocations().put(player.getUniqueId(), previousLocation);
